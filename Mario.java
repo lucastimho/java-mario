8f78d10e0fd98084f9f7ae4ac8f832e025a781de
lucastimho <lucas.t.ho.lh@gmail.com>
2022-06-30 15:21:46 -0500

    adds tube check

--- Mario.java
+++ Mario.java
@@ -3,10 +3,24 @@ import java.util.ArrayList;
 public class Mario {
     int x;
     int y;
+    int width = 55;
+    int height = 90
     double vert_vel;
     void update()
     {
         vert_vel += 1.2;
         y += vert_vel;
+    }
+    boolean isTubeUnderMario(Mario mario, Tube tube)
+    {
+        if(mario.x < tube.x + tube.width)
+            return false;
+        if(mario.x > tube.x)
+            return false;
+        if(mario.height + mario.y < tube.y ) // assumes bigger is downward
+            return false;
+        if(mario.y > tube.y + tube.height) // assumes bigger is downward
+            return false;
+        return true;
     }
 }
