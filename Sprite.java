31676a82e74e4f114c03cfc80b59bef81b920654
lucastimho <lucas.t.ho.lh@gmail.com>
2022-07-05 17:42:54 -0500

    cleans up tube class and puts methods in sprite class

--- Sprite.java
+++ Sprite.java
@@ -1,6 +1,23 @@
 abstract class Sprite {
     int x;
     int y;
+    int width;
+    int height;
 
     abstract void update();
+    boolean isThatClickInMe(int mouse_x, int mouse_y)
+    {
+        if(mouse_x < x)
+          return false;
+        if(mouse_x > x + width)
+          return false;
+        return true;
+    }
+    boolean didIClickOnYou(int mouse_x, int mouse_y, Tube t)
+    {
+      if (mouse_x == t.x && mouse_y == t.y)
+            return true;
+      else
+            return false;
+    }
 }
--- Tube.java
+++ Tube.java
@@ -1,33 +1,18 @@
 public class Tube extends Sprite {
     int x;
     int y;
-    int width = 55;
-    int height = 400;
 
     Tube(int xx, int yy)
     {
       x = xx;
       y = yy;
+      width = 55;
+      height = 400;
     }
     Tube (Json ob)
     {
         x = (int)ob.getLong("x");
         y = (int)ob.getLong("y");
-    }
-    boolean isThatClickInMe(int mouse_x, int mouse_y)
-    {
-        if(mouse_x < x)
-          return false;
-        if(mouse_x > x + width)
-          return false;
-        return true;
-    }
-    boolean didIClickOnYou(int mouse_x, int mouse_y, Tube t)
-    {
-      if (mouse_x == t.x && mouse_y == t.y)
-            return true;
-      else
-            return false;
     }
     Json marshal()
     {
