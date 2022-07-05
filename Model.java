ed63dffcebdc6b170e8eeb4b24e03df12bdd1a47
lucastimho <lucas.t.ho.lh@gmail.com>
2022-07-05 16:51:14 -0500

    moves tube to java class for clean up

--- Model.java
+++ Model.java
@@ -4,7 +4,7 @@ import java.util.Iterator;
 class Model {
         Mario mario;
         ArrayList<Tube> tubes;
-        Goomba goomba;
+        ArrayList<Goomba> goomba;
     
         Model()
         {
@@ -64,43 +64,4 @@ class Model {
             mario.remember_state();
         }
     
-}
-class Tube {
-    int x;
-    int y;
-    int width = 55;
-    int height = 400;
-
-    Tube(int xx, int yy)
-    {
-      x = xx;
-      y = yy;
-    }
-    Tube (Json ob)
-    {
-        x = (int)ob.getLong("x");
-        y = (int)ob.getLong("y");
-    }
-    boolean isThatClickInMe(int mouse_x, int mouse_y)
-    {
-        if(mouse_x < x)
-          return false;
-        if(mouse_x > x + width)
-          return false;
-        return true;
-    }
-    boolean didIClickOnYou(int mouse_x, int mouse_y, Tube t)
-    {
-      if (mouse_x == t.x && mouse_y == t.y)
-            return true;
-      else
-            return false;
-    }
-    Json marshal()
-    {
-        Json ob = Json.newObject();
-        ob.add("x", x);
-        ob.add("y", y);
-        return ob;
-    }
 }
---
+++ Tube.java
@@ -0,0 +1,39 @@
+public class Tube {
+    int x;
+    int y;
+    int width = 55;
+    int height = 400;
+
+    Tube(int xx, int yy)
+    {
+      x = xx;
+      y = yy;
+    }
+    Tube (Json ob)
+    {
+        x = (int)ob.getLong("x");
+        y = (int)ob.getLong("y");
+    }
+    boolean isThatClickInMe(int mouse_x, int mouse_y)
+    {
+        if(mouse_x < x)
+          return false;
+        if(mouse_x > x + width)
+          return false;
+        return true;
+    }
+    boolean didIClickOnYou(int mouse_x, int mouse_y, Tube t)
+    {
+      if (mouse_x == t.x && mouse_y == t.y)
+            return true;
+      else
+            return false;
+    }
+    Json marshal()
+    {
+        Json ob = Json.newObject();
+        ob.add("x", x);
+        ob.add("y", y);
+        return ob;
+    }
+}
