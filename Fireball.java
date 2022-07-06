5bf44a5bdd5ee53c7f7f908ad9088826658aba50
lucastimho <lucas.t.ho.lh@gmail.com>
2022-07-06 18:18:36 -0500

    adds fireball class

---
+++ Fireball.java
@@ -0,0 +1,12 @@
+public class Fireball extends Sprite {
+    double horiz_vel;
+    double vert_vel;
+    Fireball()
+    {
+        width = 47;
+        height = 47;
+        horiz_vel = 10.1;
+        vert_vel = 5.7;
+    }
+    void update(){};
+}
